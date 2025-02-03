8419529c99cee73a718568023008dd238f12e837
Ricardominom <rdmm_291191@hotmail.com>
2025-02-03 15:56:29 -0600

    web

--- src/main/java/com/backend/product/ProductController.java
+++ src/main/java/com/backend/product/ProductController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://backend-manager-products.vercel.app")
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductController {
